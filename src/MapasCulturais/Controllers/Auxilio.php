2f4e0bff0a204936313df59d15d290e981ac055b
João Vitor <contato.cavalcantebarbosa@gmail.com>
2021-05-05 15:56:32 -0300

    updates

--- src/MapasCulturais/Controllers/Auxilio.php
+++ src/MapasCulturais/Controllers/Auxilio.php
@@ -1850,6 +1850,7 @@ class Auxilio extends \MapasCulturais\Controllers\Registration {
                         
                          //Pega o CPF da inscrição
                          $cpf_cnpj = $this->getLineData($r, 19, 31);
+                         $cpf_cnpj_unformatted = $this->getLineData($r, 19, 31);
                          $result['LOTE_3'][$cont] = $this->validatedCanb($code, $seg, $cpf_cnpj, $inscri, $lote);
                          
                          //Firmata o CPF ou CNPJ
@@ -1863,7 +1864,7 @@ class Auxilio extends \MapasCulturais\Controllers\Registration {
                              $inscri = $this->getLineData($r, 33, 62);
 
                          }else{
-                             $inscri = $conn->fetchColumn("select id from registration where agents_data::json->'owner'->>'documento' like '%$cpf_cnpj%' AND opportunity_id = {$opportunity->id}");
+                            $inscri = $conn->fetchColumn("select id from registration where agents_data::json->'owner'->>'documento' like any(array['%$cpf_cnpj%', '%$cpf_cnpj_unformatted%']) AND opportunity_id = {$opportunity->id}");
 
                          }
                          $result['LOTE_3'][$cont] = $this->validatedCanb($code, $seg, $cpf_cnpj,  $inscri, $lote);
